0fb6edf5bdd441b7ad38f2203c48b6da583b23aa
epatters <ejpatters@gmail.com>
2017-09-24 16:07:41 -0500

    Semantic graph: Look for object annotation on ports, not wires.
    See previous commit.

--- src/FlowGraph.jl
+++ src/FlowGraph.jl
@@ -14,19 +14,19 @@ using ..Ontology
 ################
 
 @with_kw struct RawNode
-  annotation::Nullable{String} = Nullable{String}()
   language::Dict{String,Any} = Dict{String,Any}()
+  annotation::Nullable{String} = Nullable{String}()
   slot::Bool = false
 end
 
 @with_kw struct RawPort
-  annotation::Nullable{Int} = Nullable{Int}()
   language::Dict{String,Any} = Dict{String,Any}()
+  annotation::Nullable{String} = Nullable{String}()
+  index::Nullable{Int} = Nullable()
   value::Nullable = Nullable()
 end
 
 @with_kw struct RawWire
-  annotation::Nullable{String} = Nullable{String}()
   language::Dict{String,Any} = Dict{String,Any}()
   id::Nullable{String} = Nullable{String}()
   value::Nullable = Nullable()
@@ -43,20 +43,21 @@ read_raw_graph_file(args...) = read_raw_graph(LightXML.parse_file(args...))
 function GraphML.convert_from_graphml_data(::Type{RawNode}, data::Dict)
   annotation = Nullable{String}(pop!(data, "annotation", nothing))
   slot = haskey(data, "slot")
-  RawNode(annotation, data, slot)
+  RawNode(data, annotation, slot)
 end
 
 function GraphML.convert_from_graphml_data(::Type{RawPort}, data::Dict)
-  annotation = Nullable{Int}(pop!(data, "annotation", nothing))
+  annotation = Nullable{String}(pop!(data, "annotation", nothing))
+  annotation_index = Nullable{Int}(pop!(data, "annotation_index", nothing))
   value = pop!(data, "value", Nullable())
-  RawPort(annotation, data, value)
+  RawPort(data, annotation, annotation_index, value)
 end
 
 function GraphML.convert_from_graphml_data(::Type{RawWire}, data::Dict)
-  annotation = Nullable{String}(pop!(data, "annotation", nothing))
+  pop!(data, "annotation", nothing) # Get object annotation from port, not wire.
   id = Nullable{String}(pop!(data, "id", nothing))
   value = pop!(data, "value", Nullable())
-  RawWire(annotation, data, id, value)
+  RawWire(data, id, value)
 end
 
 # Support JSON data in GraphML.
@@ -108,16 +109,10 @@ function expand_annotated_box(db::OntologyDB, node::RawNode,
   note = annotation(db, get(node.annotation))::HomAnnotation
   f = WiringDiagram(inputs, outputs)
   v = add_box!(f, to_wiring_diagram(note.definition))
-  for (i, port) in enumerate(inputs)
-    if !isnull(port.annotation)
-      add_wire!(f, (input_id(f), i) => (v, get(port.annotation)))
-    end
-  end
-  for (i, port) in enumerate(outputs)
-    if !isnull(port.annotation)
-      add_wire!(f, (v, get(port.annotation)) => (output_id(f), i))
-    end
-  end
+  add_wires!(f, ((input_id(f), i) => (v, get(port.index))
+                 for (i, port) in enumerate(inputs) if !isnull(port.index)))
+  add_wires!(f, ((v, get(port.index)) => (output_id(f), i)
+                 for (i, port) in enumerate(outputs) if !isnull(port.index)))
   substitute!(f, v)
   return f
 end
--- test/FlowGraph.jl
+++ test/FlowGraph.jl
@@ -15,11 +15,16 @@ const py_data_dir = abspath(joinpath(
   @__DIR__, "..", "lang", "python", "opendisc", "integration_tests", "data"))
 
 diagram = read_raw_graph_file(joinpath(py_data_dir, "pandas_read_sql.xml"))
-@test [ get(box.value.annotation, nothing) for box in boxes(diagram) ] ==
-      [ nothing, "python/pandas/read-sql-table" ]
-@test [ get(wire.value.annotation, nothing) for wire in wires(diagram) ] ==
-      [ "python/sqlalchemy/engine", "python/sqlalchemy/engine",
-        "python/pandas/data-frame" ]
+@test nboxes(diagram) == 2
+b1, b2 = boxes(diagram)
+@test isnull(b1.value.annotation)
+@test get(b2.value.annotation) == "python/pandas/read-sql-table"
+@test [ get(p.annotation, nothing) for p in output_ports(b1) ] ==
+  [ "python/sqlalchemy/engine" ]
+@test [ get(p.annotation, nothing) for p in input_ports(b2)[1:2] ] ==
+  [ nothing, "python/sqlalchemy/engine" ]
+@test [ get(p.annotation, nothing) for p in output_ports(b2) ] ==
+  [ "python/pandas/data-frame" ]
 
 # Semantic flow graph
 #####################
@@ -29,22 +34,23 @@ db = OntologyDB()
 load_ontology_file(db, db_filename)
 
 # Convenience methods to create raw boxes and wires with or without annotations.
-raw_box(nin::Int, nout::Int) = Box(RawNode(), raw_ports(nint), raw_ports(nout))
-raw_box(annotation::String, inputs::Vector{Int}, outputs::Vector{Int}) =
-  Box(RawNode(annotation=annotation), raw_ports(inputs), raw_ports(outputs))
-raw_ports(n::Int) = [ RawPort() for i in 1:n ]
-raw_ports(v::Vector{Int}) = [ RawPort(annotation=i) for i in v ]
-raw_wire(src, tgt; kw...) = Wire(RawWire(; kw...), src, tgt)
-raw_wire(pair::Pair; kw...) = Wire(RawWire(; kw...), pair)
+const prefix = "opendisc/employee"
 add_raw_box!(f::WiringDiagram, args...) = add_box!(f, raw_box(args...))
-add_raw_wire!(f::WiringDiagram, args...; kw...) =
-  add_wire!(f, raw_wire(args...; kw...))
+raw_box(inputs, outputs) = Box(RawNode(), raw_ports(inputs), raw_ports(outputs))
+raw_box(name::String, inputs, outputs) =
+  Box(RawNode(annotation="$prefix/$name"), raw_ports(inputs), raw_ports(outputs))
+raw_ports(n::Int) = [ RawPort() for i in 1:n ]
+raw_ports(xs::Vector) = [ raw_port(x) for x in xs ]
+raw_port(name::String) = RawPort(annotation="$prefix/$name")
+raw_port(name::String, index::Int) =
+  RawPort(annotation="$prefix/$name", index=index)
+raw_port(args::Tuple) = raw_port(args...)
 
 # Expand single annotated node.
-f = WiringDiagram(raw_ports(1), raw_ports(1))
-v = add_raw_box!(f, "opendisc/employee/manager", [1], [1])
-add_raw_wire!(f, (input_id(f),1) => (v,1), annotation="opendisc/employee/employee")
-add_raw_wire!(f, (v,1) => (output_id(f),1), annotation="opendisc/employee/employee")
+f = WiringDiagram(raw_ports(["employee"]), raw_ports(["employee"]))
+v = add_raw_box!(f, "manager", [("employee",1)], [("employee",1)])
+add_wire!(f, (input_id(f),1) => (v,1))
+add_wire!(f, (v,1) => (output_id(f),1))
 actual = to_semantic_graph(db, f)
 target = to_wiring_diagram(concept(db, "reports-to"))
 @test actual == target
