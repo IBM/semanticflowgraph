bae4ca99975ea4d09fdf07bb6ebe9c6c57d2e849
epatters <ejpatters@gmail.com>
2017-10-03 18:29:42 -0500

    TST: Semantic graph: sklearn regression integration test now passing.

--- test/integration/FlowGraph.jl
+++ test/integration/FlowGraph.jl
@@ -40,7 +40,7 @@ load_ontology_file(db, dso_filename)
 raw = read_py_raw_graph("pandas_read_sql")
 semantic = to_semantic_graph(db, raw; elements=false)
 d = WiringDiagram([], concepts(db, ["table"]))
-engine = add_box!(d, Box(nothing, [], concepts(db, ["sql-database"])))
+engine = add_box!(d, Box([], concepts(db, ["sql-database"])))
 cons = add_box!(d, construct(pair(concept(db, "sql-table-database"),
                                   concept(db, "sql-table-name"))))
 read = add_box!(d, concept(db, "read-table"))
@@ -58,8 +58,7 @@ d = WiringDiagram([], concepts(db, ["array"]))
 filename = add_box!(d, construct(concept(db, "filename")))
 read = add_box!(d, concept(db, "read-tabular-file"))
 kmeans = add_box!(d, construct(concept(db, "k-means")))
-transform = add_box!(d, Box(
-  nothing, concepts(db, ["table"]), concepts(db, ["array"])))
+transform = add_box!(d, Box(concepts(db, ["table"]), concepts(db, ["array"])))
 fit = add_box!(d, concept(db, "fit"))
 clusters = add_box!(d, concept(db, "clustering-model-clusters"))
 add_wires!(d, [
@@ -73,20 +72,20 @@ add_wires!(d, [
 @test semantic == d
 
 # Compare sklearn clustering models using a cluster similarity metric.
+# FIXME: The boxes are added to `d` in the exact order of `semantic`. That
+# won't be necessary when we implement graph isomorphism for wiring diagrams.
 raw = read_py_raw_graph("sklearn_clustering_metrics")
 semantic = to_semantic_graph(db, raw; elements=false)
 clustering_fit = Hom("fit",
   otimes(concept(db, "clustering-model"), concept(db, "data")),
   concept(db, "clustering-model"))
-# XXX: The boxes are added to `d` in the exact order of `semantic`. That won't
-# be necessary when we implement graph isomorphism for wiring diagrams.
 d = WiringDiagram([], concepts(db, ["array", "k-means", "agglomerative-clustering"]))
-make_data = add_box!(d, Box(nothing, [], concepts(db, ["array", "array"])))
+make_data = add_box!(d, Box([], concepts(db, ["array", "array"])))
 kmeans_fit = add_box!(d, clustering_fit)
 agglom = add_box!(d, construct(concept(db, "agglomerative-clustering")))
 agglom_clusters = add_box!(d, concept(db, "clustering-model-clusters"))
 agglom_fit = add_box!(d, clustering_fit)
-score = add_box!(d, Box(nothing, concepts(db, ["array", "array"]), []))
+score = add_box!(d, Box(concepts(db, ["array", "array"]), []))
 kmeans = add_box!(d, construct(concept(db, "k-means")))
 kmeans_clusters = add_box!(d, concept(db, "clustering-model-clusters"))
 add_wires!(d, [
@@ -101,6 +100,35 @@ add_wires!(d, [
   (agglom_fit, 1) => (agglom_clusters, 1),
   (agglom_fit, 1) => (output_id(d), 3),
   (agglom_clusters, 1) => (score, 2),
+])
+@test semantic == d
+
+# Errors metrics for linear regression using sklearn.
+raw = read_py_raw_graph("sklearn_regression_metrics")
+semantic = to_semantic_graph(db, raw; elements=false)
+d = WiringDiagram([], [])
+filename = add_box!(d, construct(concept(db, "filename")))
+data_x = add_box!(d, Box(concepts(db, ["table"]), concepts(db, ["table"])))
+data_y = add_box!(d, Box(concepts(db, ["table"]), concepts(db, ["column"])))
+ols = add_box!(d, construct(concept(db, "least-squares")))
+fit = add_box!(d, concept(db, "fit-supervised"))
+predict = add_box!(d, concept(db, "predict"))
+error_l1 = add_box!(d, concept(db, "mean-absolute-error"))
+error_l2 = add_box!(d, concept(db, "mean-squared-error"))
+read = add_box!(d, concept(db, "read-tabular-file"))
+add_wires!(d, [
+  (filename, 1) => (read, 1),
+  (read, 1) => (data_x, 1),
+  (read, 1) => (data_y, 1),
+  (ols, 1) => (fit, 1),
+  (data_x, 1) => (fit, 2),
+  (data_y, 1) => (fit, 3),
+  (fit, 1) => (predict, 1),
+  (data_x, 1) => (predict, 2),
+  (data_y, 1) => (error_l1, 1),
+  (predict, 1) => (error_l1, 2),
+  (data_y, 1) => (error_l2, 1),
+  (predict, 1) => (error_l2, 2),
 ])
 @test semantic == d
 
