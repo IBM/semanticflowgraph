5cb29f253b4f5dfe9102b4e4f3b3e444513d5b9b
epatters <ejpatters@gmail.com>
2017-11-08 23:24:53 -0600

    Fix CouchDB version requirement in setup.py.
    We need any build strictly newer than the currently released version 1.11.

--- lang/python/setup.py
+++ lang/python/setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup_args = {
     'name': 'opendisc',
     'version': '0.1',
-    'description': 'Open Discovery prototype',
+    'description': 'Open Discovery program analysis for Python',
     'include_package_data': True,
     'packages': find_packages(),
     'zip_safe': False,
     'author': 'Evan Patterson',
     'author_email': 'evan.patterson@ibm.com',
-    'install_requires': [ # core package
+    'install_requires': [
+        # core package
         'pathlib2',
         'six',
         'traitlets',
@@ -17,7 +18,7 @@ setup_args = {
         'networkx==1.11',
         'cachetools>=2.0.0',
         'blitzdb>=0.3',
-        'CouchDB>=1.1.1',
+        'CouchDB>1.1',
         'sqlalchemy',
         'ipykernel>=4.3.0',
     ],
