f91e28a8d20e93aa2cbba91de82fb8c5d11494d4
epatters <ejpatters@gmail.com>
2018-05-08 18:58:09 -0500

    TST: Actually check semantic graph in k-means in R integration test.

--- test/integration/RFlowGraphs.jl
+++ test/integration/RFlowGraphs.jl
@@ -44,6 +44,29 @@ function create_r_semantic_graph(db::OntologyDB, name::String; kw...)
 end
 
 # K-means clustering on the Iris dataset using base R.
+# FIXME: The encapsulated box should only have one input port.
+# FIXME: Box order, as noted in PyFlowGraphs.
 semantic = create_r_semantic_graph(db, "clustering_kmeans"; elements=false)
+d = WiringDiagram([], [])
+kmeans = add_box!(d, construct(pair(concepts(db,
+  ["k-means", "clustering-model-n-clusters"])...)))
+read = add_box!(d, concept(db, "read-tabular-file"))
+file = add_box!(d, construct(pair(concepts(db, ["tabular-file", "filename"])...)))
+centroids = add_box!(d, concept(db, "k-means-centroids"))
+clusters = add_box!(d, concept(db, "clustering-model-clusters"))
+fit = add_box!(d, Hom("fit",
+  otimes(concept(db, "k-means"), concept(db, "data")),
+  concept(db, "k-means")))
+transform = add_box!(d, Box(concepts(db, ["table","table"]), concepts(db, ["table"])))
+add_wires!(d, [
+  (file, 1) => (read, 1),
+  (read, 1) => (transform, 1),
+  (read, 1) => (transform, 2),
+  (kmeans, 1) => (fit, 1),
+  (transform, 1) => (fit, 2),
+  (fit, 1) => (clusters, 1),
+  (fit, 1) => (centroids, 1),
+])
+@test semantic == d
 
 end
