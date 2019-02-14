f0b51d20e984df44a2c889a927ece14ae9b989cc
Evan Patterson <ejpatters@gmail.com>
2019-02-14 11:15:32 -0800

    ENH: CLI option to use local annotations when recording R code.

--- src/extras/CLI-R.jl
+++ src/extras/CLI-R.jl
@@ -4,5 +4,6 @@ using .RCall
 
 function record_file(inpath::String, outpath::String, args::Dict, ::Val{:r})
   RFlowGraphs.record_file(
-    inpath, out=outpath, cwd=dirname(inpath), annotate=true)
+    inpath, out=outpath, cwd=dirname(inpath), annotate=true,
+    annotations=args["annotations"])
 end
