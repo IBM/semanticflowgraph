9a5156297cfcf1bdc43545ae4c93bfc872303f68
epatters <ejpatters@gmail.com>
2019-09-09 21:53:23 -0700

    CLEANUP: Don't use deprecated macro `@pyimport`.

--- src/extras/CLI-Python.jl
+++ src/extras/CLI-Python.jl
@@ -1,16 +1,15 @@
 using Compat
 using .PyCall
 
-@pyimport flowgraph.core.record as PyFlowGraphs
-@pyimport flowgraph.core.annotation_db as PyAnnotationDBs
+const PyFlowGraphs = pyimport("flowgraph.core.record")
+const PyAnnotationDBs = pyimport("flowgraph.core.annotation_db")
 
 function record_file(inpath::String, outpath::String, args::Dict, ::Val{:python})
   if isnothing(args["annotations"])
     db = nothing
   else
-    # XXX: Why can't I just write `db.load_file`?
     db = PyAnnotationDBs.AnnotationDB()
-    py"$(db).load_file"(abspath(args["annotations"]))
+    db.load_file(abspath(args["annotations"]))
   end
   PyFlowGraphs.record_script(
     inpath, out=outpath, cwd=dirname(inpath), db=db,
